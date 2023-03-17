a81a57dc4f808c3b47cf0366b008bea26ca50661
SheardK <sheardokumu@gmail.com>
2023-03-17 23:01:18 +0300

    Comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,30 +1,28 @@
-/*
- * File: 9-print_comb.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main- Entry point
+ * @void: Null value
  *
- * Return: Always 0.
+ * Description: Print numbers from 0 to 9 separated by comma and space
+ * Return: Zero value
  */
+
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 0; num <= 9; num++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
+Footer
+
